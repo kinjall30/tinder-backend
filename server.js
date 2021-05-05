6ce9cf5ee2743f7ef8d5e1ae25ff16a5b15e6f99
KINJAL PRAJAPATI <77926892+kinjall30@users.noreply.github.com>
2021-05-05 18:35:54 +0530

    tinder-backend

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cards from './dbCards.js';
+import Cors from 'cors'; 
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = 'mongodb+srv://admin:G4tH5yWvDrXrqknk@cluster0.flyod.mongodb.net/tinder-db?retryWrites=true&w=majority'
+
+// Middleware
+app.use(express.json());
+app.use(Cors());
+
+// DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello kinjal!!'));
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    cards.create(dbCard, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get("/tinder/cards", (req, res) => {
+   cards.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    }); 
+});
+
+// Listener
+app.listen(port, () => console.log(`listining on loocalhost: ${port}`));
